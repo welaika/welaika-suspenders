380f5da0f04e36be9eaf3ac62f20ca6f94b0ccb5
Stefano Verna <stefano.verna@welaika.com>
2013-03-28 17:01:23 +0100

    Fixes

--- templates/model_spec_helper.rb
+++ templates/model_spec_helper.rb
@@ -14,3 +14,9 @@ autoload_paths = ActiveSupport::Dependencies.autoload_paths
   autoload_paths.push(path) unless autoload_paths.include?(path)
 end
 
+# find definitions only if factory girls has not been already required
+if require 'factory_girl'
+  require File.join(RAILS_ROOT, "spec/support/factory_girl.rb")
+  FactoryGirl.find_definitions
+end
+
--- templates/presenters.rb
+++ templates/presenters.rb
@@ -1,3 +1,1 @@
-ActionController::Base.send :include, BasicPresenter::Helpers
-ActionController::Base.send :helper_method, :present
-ActionController::Base.send :helper_method, :present_collection
+ActionController::Base.send :helper, BasicPresenter::Helpers
