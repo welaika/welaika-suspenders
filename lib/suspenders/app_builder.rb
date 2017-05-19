bfa913bd74096003b82fd587a86e0befbc8f1d2f
Fabrizio Monti <fabrizio.monti@welaika.com>
2017-05-19 17:38:47 +0200

    OPS... there were two methods with the same name

--- lib/suspenders/app_builder.rb
+++ lib/suspenders/app_builder.rb
@@ -399,10 +399,6 @@ you can deploy to staging and production with:
       run "chmod a+x bin/deploy"
     end
 
-    def setup_bundler_audit
-      copy_file "bundler_audit.rake", "lib/tasks/bundler_audit.rake"
-    end
-
     def setup_brakeman
       copy_file "brakeman.rake", "lib/tasks/brakeman.rake"
     end
