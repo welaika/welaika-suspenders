d73b3b9b7df8cf3eab269bce5c8bd64f682a9f8d
Fabrizio Monti <fabrizio.monti@welaika.com>
2018-07-08 16:29:08 +0200

    Fix typo

--- lib/suspenders/generators/security_generator.rb
+++ lib/suspenders/generators/security_generator.rb
@@ -8,7 +8,7 @@ module Suspenders
     )
 
     def add_checkers_gems
-      gem 'brakeman', require: false, groupd: :development
+      gem 'brakeman', require: false, group: :development
       gem 'bundler-audit', '>= 0.5.0', require: false, group: :development
       Bundler.with_clean_env { run "bundle install" }
     end
