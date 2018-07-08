cd9189fefdf5456b1eb5a7dadb94b6f705836735
Fabrizio Monti <fabrizio.monti@welaika.com>
2018-07-08 22:06:06 +0200

    Do not load sentry when in dev or test environment

--- templates/sentry.rb
+++ templates/sentry.rb
@@ -1,6 +1,8 @@
-Raven.configure do |config|
-  config.dsn = ENV.fetch('SENTRY_DSN')
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-  config.environments = %w[ staging production ]
-  config.current_environment = ENV.fetch('SENTRY_CURRENT_ENV')
+unless Rails.env.development? || Rails.env.test?
+  Raven.configure do |config|
+    config.dsn = ENV.fetch('SENTRY_DSN')
+    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+    config.environments = %w[ staging production ]
+    config.current_environment = ENV.fetch('SENTRY_CURRENT_ENV')
+  end
 end
