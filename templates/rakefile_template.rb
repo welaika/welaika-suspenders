0bba6b7cb0bbe0bc17707c8e74e165bd2764bc18
Fabrizio Monti <fabrizio.monti@welaika.com>
2018-08-05 19:14:15 +0200

    Setup the test db before trying to migrate it

--- templates/rakefile_template.rb
+++ templates/rakefile_template.rb
@@ -14,7 +14,8 @@ task :default do
   sh 'bundle exec rubycritic app/ config/ lib/ --no-browser --minimum-score 94 --format console'
   sh 'bundle exec bundle-audit check --update'
   sh 'bundle exec brakeman -z'
-  sh 'bundle exec rails db:migrate RAILS_ENV=test'
+  sh 'bundle exec rails db:setup RAILS_ENV=test'
+  sh 'bundle exec rails db:migrate RAILS_ENV=test' # runs any pending migrations
   sh 'bundle exec rake factory_bot:lint RAILS_ENV=test'
   sh 'bundle exec rspec spec/'
 end
