e98ac94dcc3514de6a0e2468cbcd4a9cb9115df8
Fabrizio Monti <fabrizio.monti@welaika.com>
2018-08-06 00:06:46 +0200

    Update banner

--- lib/suspenders/generators/app_generator.rb
+++ lib/suspenders/generators/app_generator.rb
@@ -191,7 +191,7 @@ module Suspenders
     end
 
     def self.banner
-      "suspenders #{arguments.map(&:usage).join(' ')} [options]"
+      "welaika-suspenders #{arguments.map(&:usage).join(' ')} [options]"
     end
 
     protected
