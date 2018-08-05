b9d0c3989e32b29fc3e3dcf90178efeca6cbe602
Fabrizio Monti <fabrizio.monti@welaika.com>
2018-08-05 19:13:58 +0200

    Please the rubocop

--- templates/rails_helper.rb
+++ templates/rails_helper.rb
@@ -4,7 +4,6 @@ ENV['RACK_ENV'] = 'test'
 
 require File.expand_path('../config/environment', __dir__)
 
-
 abort('DATABASE_URL environment variable is set') if ENV['DATABASE_URL']
 
 require 'rspec/rails'
