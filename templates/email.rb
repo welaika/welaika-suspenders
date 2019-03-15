b2d948aa63d9bfbac8ea6b8d91871f884a1b115e
Fabrizio Monti <fabrizio.monti@welaika.com>
2019-03-15 19:56:55 +0100

    Add a prefix to email interceptor emails

--- templates/email.rb
+++ templates/email.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 if ENV["EMAIL_RECIPIENTS"].present?
-  Mail.register_interceptor RecipientInterceptor.new(ENV["EMAIL_RECIPIENTS"])
+  Mail.register_interceptor(
+    RecipientInterceptor.new(
+      ENV["EMAIL_RECIPIENTS"],
+      subject_prefix: '[STAGING]'
+    )
+  )
 end
