bfe0deba595ba938562f765dd58482dc0b8fd481
Fabrizio Monti <fabrizio.monti@welaika.com>
2019-06-24 17:28:22 +0200

    Add Dockerfile for gitlab ci

--- lib/suspenders/generators/ci_generator.rb
+++ lib/suspenders/generators/ci_generator.rb
@@ -4,7 +4,9 @@ module Suspenders
   class CiGenerator < Generators::Base
     def configure_ci
       template 'gitlab-ci.yml.erb', '.gitlab-ci.yml'
+      template 'Dockerfile.gitlab.erb', 'Dockerfile.gitlab'
       copy_file 'database.gitlab.yml', 'config/database.gitlab.yml'
+      copy_file 'dockerignore', '.dockerignore'
     end
   end
 end
