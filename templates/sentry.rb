dd96feffb0de40de986699885f5b2439b77aadd0
Fabrizio Monti <fabrizio.monti@welaika.com>
2019-07-10 12:20:35 +0200

    Please the rubocop

--- templates/sentry.rb
+++ templates/sentry.rb
@@ -3,6 +3,6 @@
 Raven.configure do |config|
   config.dsn = ENV.fetch('SENTRY_DSN')
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-  config.environments = %w[ staging production ]
+  config.environments = %w[staging production]
   config.current_environment = ENV.fetch('SENTRY_ENV', Rails.env)
 end
