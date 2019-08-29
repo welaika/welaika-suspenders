51719db09a399bf807f974c9e0fda751d32e9e5e
Fabrizio Monti <fabrizio.monti@welaika.com>
2019-08-29 18:32:02 +0200

    Freeze SMTP Settings

--- templates/smtp.rb
+++ templates/smtp.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 SMTP_SETTINGS = {
-  address: ENV.fetch("SMTP_ADDRESS"), # example: "smtp.sendgrid.net"
+  address: ENV.fetch('SMTP_ADDRESS'), # example: "smtp.sendgrid.net"
   authentication: :login,
-  domain: ENV.fetch("SMTP_DOMAIN"), # example: "heroku.com"
+  domain: ENV.fetch('SMTP_DOMAIN'), # example: "heroku.com"
   enable_starttls_auto: true,
-  password: ENV.fetch("SMTP_PASSWORD"),
-  port: "587",
-  user_name: ENV.fetch("SMTP_USERNAME")
-}
+  password: ENV.fetch('SMTP_PASSWORD'),
+  port: '587',
+  user_name: ENV.fetch('SMTP_USERNAME')
+}.freeze
