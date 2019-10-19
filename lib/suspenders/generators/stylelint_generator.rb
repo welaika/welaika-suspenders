7bc5fba1a013786b4fe2c73b5d5714450b59c4e7
Fabrizio Monti <fabrizio.monti@welaika.com>
2019-10-19 17:11:11 +0200

    Fix style lint destroying action

--- lib/suspenders/generators/stylelint_generator.rb
+++ lib/suspenders/generators/stylelint_generator.rb
@@ -3,11 +3,13 @@ require_relative "base"
 module Suspenders
   class StylelintGenerator < Generators::Base
     def install_stylelint
-      dependencies = ["stylelint@10", # FIXME: config-recommened is not ready for stylelint 11 yet
-                      "stylelint-config-recommended",
-                      "stylelint-order",
-                      "stylelint-declaration-block-no-ignored-properties",
-                      "stylelint-scss"]
+      dependencies = [
+        { name: "stylelint", version: '10' }, # FIXME: config-recommened is not ready for stylelint 11 yet
+        { name: "stylelint-config-recommended" },
+        { name: "stylelint-order" },
+        { name: "stylelint-declaration-block-no-ignored-properties" },
+        { name: "stylelint-scss" }
+      ]
       action YarnInstall.new(self, dependencies, "--dev")
     end
 
@@ -18,17 +20,18 @@ module Suspenders
     class YarnInstall
       def initialize(base, dependencies, flags)
         @base = base
-        @dependencies = dependencies.join(" ")
+        @dependencies = dependencies
         @flags = flags
       end
 
       def invoke!
-        @base.run "bin/yarn add #{@dependencies} #{@flags}"
+        dependencies_with_versions = @dependencies.map { |d| d[:version] ? "#{d[:name]}@#{d[:version]}" : d[:name] }.join(' ')
+        @base.run "bin/yarn add #{dependencies_with_versions} #{@flags}"
       end
 
       def revoke!
         @base.behavior = :invoke
-        @base.run "bin/yarn remove #{@dependencies}"
+        @base.run "bin/yarn remove #{@dependencies.map { |d| d[:name] }.join(' ')}"
       ensure
         @base.behavior = :revoke
       end
