804a70cf5f88eda3ccf17109bd80b3ec9880e4bd
Fabrizio Monti <fabrizio.monti@welaika.com>
2019-10-19 14:43:41 +0200

    Update rubocop rules

--- templates/rack_mini_profiler.rb
+++ templates/rack_mini_profiler.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-if ENV["RACK_MINI_PROFILER"].to_i > 0
-  require "rack-mini-profiler"
+if ENV['RACK_MINI_PROFILER'].to_i.positive?
+  require 'rack-mini-profiler'
 
   Rack::MiniProfilerRails.initialize!(Rails.application)
 end
