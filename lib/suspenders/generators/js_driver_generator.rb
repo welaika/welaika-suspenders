df8bbabc97e47ecd778b1397e3e4dc6632b48677
Fabrizio Monti <fabrizio.monti@welaika.com>
2019-10-20 15:27:24 +0200

    Add rails 6 support

--- lib/suspenders/generators/js_driver_generator.rb
+++ lib/suspenders/generators/js_driver_generator.rb
@@ -3,9 +3,9 @@ require_relative "base"
 module Suspenders
   class JsDriverGenerator < Generators::Base
     def add_gems
-      gem 'capybara', '>= 2.15', '< 4.0', group: :test
+      gem 'capybara', '>= 2.15', group: :test
       gem 'selenium-webdriver', group: :test
-      gem 'webdrivers', '~> 4.0', group: :test
+      gem 'webdrivers', group: :test
       Bundler.with_clean_env { run "bundle install" }
     end
 
--- lib/suspenders/version.rb
+++ lib/suspenders/version.rb
@@ -1,9 +1,9 @@
 module Suspenders
-  RAILS_VERSION = "~> 5.2.3".freeze
+  RAILS_VERSION = "~> 6.0.0".freeze
   POSTGRES_VERSION = "11".freeze # Used in CI
   RUBY_VERSION = IO.
     read("#{File.dirname(__FILE__)}/../../.ruby-version").
     strip.
     freeze
-  VERSION = "2.36".freeze
+  VERSION = "3.0".freeze
 end
